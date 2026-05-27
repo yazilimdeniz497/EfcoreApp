65d78d8354b10a4460b99045449a7b659215415e
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-27 18:57:45 +0300

    ogrenci modeli eklendi

---
+++ EfcoreApp/Models/Ogrenci.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EfcoreApp.Models
+{
+    public class Ogrenci
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Ogrenciad { get; set; }
+        public string OgrenciSoyad { get; set; }
+        public string ImageFıle { get; set; }
+        public string Telephone { get; set; }
+        public string Email { get; set; }
+    }
+}
