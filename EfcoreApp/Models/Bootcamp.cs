0ade4c70a463211a921a546a11681863886a926c
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-27 18:58:55 +0300

    Bootcamp modeli eklendi

---
+++ EfcoreApp/Models/Bootcamp.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EfcoreApp.Models
+{
+    public class Bootcamp
+    {
+        [Key]
+        public int Id { get; set; }
+        public string BootcampName { get; set; }
+    }
+}
