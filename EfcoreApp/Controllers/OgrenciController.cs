dfe8bb51df826e910b7dc3078ae34800fcb96510
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-28 15:29:05 +0300

    ogrencicontroller ekleme sayfası eklendi

---
+++ EfcoreApp/Controllers/OgrenciController.cs
@@ -0,0 +1,19 @@
+﻿using EfcoreApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EfcoreApp.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        private readonly DataContext _context;
+        public OgrenciController(DataContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public IActionResult CreateOgrenci()
+        {
+            return View();
+        }
+    }
+}
