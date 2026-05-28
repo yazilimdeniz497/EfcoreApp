0ebf7982e599c2a01d11aaed9f25613d88b21e22
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-28 14:01:08 +0300

    datacontext eklendi

---
+++ EfcoreApp/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EfcoreApp.Models
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+        public DbSet<Ogrenci> Ogrenciler => Set<Ogrenci>();
+        public DbSet<Bootcamp> Bootcampler => Set<Bootcamp>();
+        public DbSet<BootcampKayit> BootcampKayitlar => Set<BootcampKayit>();
+        
+    }
+}
