6f0937a26da6f64a9370313b3ab17587d6e789d8
yazilimdeniz497 <yazilimdeniz497@gmail.com>
2026-05-28 11:51:17 +0300

    bootcampkayıt eklendi

---
+++ EfcoreApp/Models/BootcampKayit.cs
@@ -0,0 +1,10 @@
+﻿namespace EfcoreApp.Models
+{
+    public class BootcampKayit
+    {
+        public int KayıtId { get; set; }
+        public int BootcampId { get; set; }
+        public int OgrenciId { get; set; }
+        public DateTime kayıtTarihi { get; set; }
+    }
+}
